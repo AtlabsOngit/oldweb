1e8024b736b04f46b71c434284252dcb4410b1c4
AtlabsOngit <89631390+AtlabsOngit@users.noreply.github.com>
2022-10-27 12:48:26 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var name='Acquah Andrews';
   return (
     <div className="App">
    <h1>Hello World</h1>
     <h2>Adding a new test</h2>
+    <h2>the new name is {name} </h2>
     </div>
   );
 }
